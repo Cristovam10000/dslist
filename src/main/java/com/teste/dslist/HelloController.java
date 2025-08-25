6195720562ac607f201ad6a35cf0a6beded71fef
cristovam <cristovam1712@gmail.com>
2025-08-25 12:35:21 -0300

    first commit

---
+++ src/main/java/com/teste/dslist/HelloController.java
@@ -0,0 +1,13 @@
+package com.teste.dslist;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "Spring Boot está ativo";
+    }
+}
