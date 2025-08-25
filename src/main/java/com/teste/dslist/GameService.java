c002515dda2fec337a129df09eace978062a9e2e
cristovam <cristovam1712@gmail.com>
2025-08-25 18:09:48 -0300

    adiciona injeção de dependência para GameRepository em GameService

--- src/main/java/com/teste/dslist/GameService.java
+++ src/main/java/com/teste/dslist/GameService.java
@@ -1,12 +1,18 @@
 package com.teste.dslist;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
 
+    @Autowired
+    private GameRepository gameRepository;
+
     public List<GameMinDTO> findAll() {
-        
+        List<Game> result = gameRepository.findAll();
+        return result;
     }
 }
