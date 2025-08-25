37b81c6b9f0a7ce37b311098b3735350cc31516c
cristovam <cristovam1712@gmail.com>
2025-08-25 18:00:44 -0300

    adiciona classes GameMinDTO, GameRepository e GameService

---
+++ src/main/java/com/teste/dslist/GameRepository.java
@@ -0,0 +1,8 @@
+ package com.teste.dslist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+    
+    
+} 
---
+++ src/main/java/com/teste/dslist/GameService.java
@@ -0,0 +1,12 @@
+package com.teste.dslist;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+
+    public List<GameMinDTO> findAll() {
+        
+    }
+}
